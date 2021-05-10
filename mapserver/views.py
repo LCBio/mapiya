ca1c09e9ddde76b0fc63b1367e8217ac968791de
Mateusz Kurcinski <mkurcinski@gmail.com>
2021-05-10 21:43:42 +0200

    revert changes in plotly_apps and views back to d6d2a71

--- mapserver/views.py
+++ mapserver/views.py
@@ -23,7 +23,6 @@ def get_identity(request):
         request.session.save()
         return models.Identity.objects.get_or_create(session_id=request.session.session_key)[0]
 
-
 class Home(SingleTableView):
 
     table_class = tables.MapTable
@@ -58,10 +57,9 @@ class Detail(generic.DetailView):
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
         data['table'] = tables.NGLTable(mapobj=self.object)
-        data['plotly'] = {
-            'input-path': {'value': self.object.pdb.name},
-            'input-info': {'value': self.object.mapmodel_set.first().info}
-        }
+        path = str(kwargs['object'].pdb)
+        info=str(models.MapModel.objects.get(matrix=path.split('/')[0]+'/'+path.split('/')[1]+'/matrix0.npy').info)
+        data['plotly'] = {'input-path': {'value': path}, 'input-info': {'value': info}}
         return data
 
 
