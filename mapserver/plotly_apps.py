1ded93571adc52f20e8a071c5c8405b5f106b235
aedawid <dawid.aleksandra@gmail.com>
2021-05-28 09:24:32 -0500

    unnecessary dependencies have been removed (#include dash_bio, pandas, etc)

--- mapserver/plotly_apps.py
+++ mapserver/plotly_apps.py
@@ -1,10 +1,7 @@
 import os
 import json
 import numpy as np
-from datetime import datetime
-import pandas as pd
 import dash
-import dash_bio as dashbio
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objects as go
