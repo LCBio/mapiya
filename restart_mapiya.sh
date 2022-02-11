28da856794f0ac29b3ec855645903334d94014e8
mapserver <admin@lcbio.pl>
2022-02-11 19:02:43 +0100

    add run_workers.sh

--- restart_mapiya.sh
+++ restart_mapiya.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 killall gunicorn
+ps ax | grep rqworker | grep -v grep | awk '{print $1}' | xargs kill
 
 source /home/mapserver/miniconda3/bin/activate venv
 export TMPDIR=/home/mapserver/tmp
 gunicorn -c lcbio/gunicorn.py
+/home/mapserver/mapserver/run_workers.sh 10
