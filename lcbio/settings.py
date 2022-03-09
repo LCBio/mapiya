227d366a7f7b2dbdfea86ae0a34ed67d2c64fc20
Mateusz Kurcinski <mkurcinski@gmail.com>
2022-03-09 10:33:02 +0100

    add mapiya.lcbio.pl to trustworthy csrf hosts

--- lcbio/settings.py
+++ lcbio/settings.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0sdys*-h6wn3fk2)!cjnmu(c%^p-%*c4m13)s2xk2ou5u_g0_x'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['10.10.13.214']
 ROOT_URLCONF = 'lcbio.urls'
 WSGI_APPLICATION = 'lcbio.wsgi.application'
@@ -21,6 +21,7 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 DATA_UPLOAD_MAX_MEMORY_SIZE = 100000000
+CSRF_TRUSTED_ORIGINS = ['http://mapiya.lcbio.pl', 'http://10.10.13.214']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
