d81f872437b9c013f069d354222841a7d0d95421
Mateusz Kurcinski <mkurcinski@gmail.com>
2022-04-01 11:23:22 +0200

    remove plotly2 app

--- mapserver/views.py
+++ mapserver/views.py
@@ -58,17 +58,6 @@ class Detail(generic.DetailView):
         return data
 
 
-class PlotlyProject(generic.DetailView):
-
-    model = models.Project
-    template_name = 'plotly.html'
-
-    def get_context_data(self, **kwargs):
-        data = super().get_context_data(**kwargs)
-        data['plotly_args'] = {'project-pk': {'value': self.object.pk}}
-        return data
-
-
 class Delete(generic.DeleteView):
 
     model = models.Project
